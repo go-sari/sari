14220910adb9b5893eb180bf2cabab046d31f290
Eliezio Oliveira <eliezio@pm.me>
2020-05-08 00:18:48 +0100

    Add support to remote backend

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,13 @@ set -eux
 
 PULUMI_ACTION=$1
 
-pulumi login --local
-pulumi stack init this
+if [ -n "$PULUMI_BACKEND_URL" ]; then
+    pulumi login --cloud-url $PULUMI_BACKEND_URL
+else
+    pulumi login --local
+fi
+
+[ -f Pulumi.this.yaml ] || pulumi stack init this
 if [ -n "$SOCKS5_PROXY" ]; then
     pulumi config set socks5_proxy $SOCKS5_PROXY
 fi
