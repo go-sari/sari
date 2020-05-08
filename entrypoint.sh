7289060253ccad6b14987626a06b8a7693a4daac
Eliezio Oliveira <eliezio@pm.me>
2020-05-08 11:05:16 +0100

    Fix ref to optional EnvVars in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,14 +4,14 @@ set -eux
 
 PULUMI_ACTION=$1
 
-if [ -n "$PULUMI_BACKEND_URL" ]; then
+if [ -n "${PULUMI_BACKEND_URL:-}" ]; then
     pulumi login --cloud-url $PULUMI_BACKEND_URL
 else
     pulumi login --local
 fi
 
 [ -f Pulumi.this.yaml ] || pulumi stack init this
-if [ -n "$SOCKS5_PROXY" ]; then
+if [ -n "${SOCKS5_PROXY:-}" ]; then
     pulumi config set socks5_proxy $SOCKS5_PROXY
 fi
 pulumi $PULUMI_ACTION
