fbb9801a12da1954a0a8e8edac95eed6bb10dd66
Eliezio Oliveira <eliezio@pm.me>
2020-05-23 12:29:00 +0100

    entrypoint.sh: Stack name taken from PULUMI_STACK_NAME

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,6 +11,6 @@ else
     pulumi --non-interactive login --local
 fi
 
-pulumi --non-interactive stack select this --create
+pulumi --non-interactive stack select $PULUMI_STACK_NAME --create
 pulumi --non-interactive stack export | jq '[.deployment.resources[]? | .urn]' > resources.json
 pulumi --non-interactive --logtostderr -v=${PULUMI_LOG_LEVEL:-2} ${PULUMI_ACTION:-preview}
